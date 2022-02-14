b9cd6fd42702de5d58c8111aa9f1e87ea7542ac5
= <=>
2022-02-14 12:30:09 +0700

    Update kelas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-    <h1> DELLA JANNATA FEBIANA :) <h1>
+    <h1> DELLA FEBIANA :) <h1>
         <h2 align=center> D4-TI2E <h2>
         </div>
         </div>
